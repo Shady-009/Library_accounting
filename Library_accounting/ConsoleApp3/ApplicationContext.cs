82cc38c3057cbb77d8c43a9fc537d2c94877492a
Aleksandr <81921344+Shady-009@users.noreply.github.com>
2021-05-28 00:30:45 +0300

    Add files via upload

---
+++ Library_accounting/ConsoleApp3/ApplicationContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Reader> Readers { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Rent> BookRent { get; set; }
+
+        public ApplicationContext()
+        {
+            Database.EnsureDeleted(); 
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=librarydb;Trusted_Connection=True;");
+        }
+    }
+}
